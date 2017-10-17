17e5f57e88164293b106a7087f97bbfa7980d4db
wunami <wunami@users.noreply.github.com>
2017-10-16 21:55:08 -0700

    Create finger.php
    a page for the fingerprint application to send info to

---
+++ finger.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Metrical Fingerprint Basic Test</title>
+</head>
+<?php $Identity = $_REQUEST['Identity'] ; $Location = $_REQUEST['Location'] ; ?>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<p>
+Identity data:&nbsp;
+<?php print $Identity; ?>
+<p>
+From location:&nbsp;
+<?php print $Location; ?>
+<p>
+</body>
+</html>
