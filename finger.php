a18915c40207bfa5165f1960ab06f9f44d032b8e
wunami <wunami@users.noreply.github.com>
2017-10-31 16:17:46 -0700

    Update finger.php

--- finger.php
+++ finger.php
@@ -5,12 +5,22 @@
 <?php $Identity = $_REQUEST['Identity'] ; $Location = $_REQUEST['Location'] ; ?>
 
 <body bgcolor="#FFFFFF" text="#000000">
+<center>
+<br><br><br>
 <p>
 Identity data:&nbsp;
 <?php print $Identity; ?>
 <p>
 From location:&nbsp;
 <?php print $Location; ?>
+<?php 
+if ($Identity == "007") {
+	echo "<body style='background-color:green'>";
+} else {
+	echo "<body style='background-color:red'>";
+}
+?>
+<center>
 <p>
 </body>
 </html>
