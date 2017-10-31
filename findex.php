dbc56931637c781883eb4784cde5d6c40cb09a4c
wunami <wunami@users.noreply.github.com>
2017-10-31 16:32:50 -0700

    Add files via upload

---
+++ findex.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Metrical Fingerprint Basic Test</title>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<center>
+<div style="display:table; width:100%; height:100%;">
+    <div style="display:table-cell; vertical-align:middle;">
+        <img src="metrical3.png">
+        <p><br><br>Ready!</p>
+    </div>
+</div>
+</center>
+</body>
+</html>
