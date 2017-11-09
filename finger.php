f787db1395c5d21ae26ba08ad5b2d6e6a3809671
wunami <wunami@users.noreply.github.com>
2017-11-09 12:00:12 -0800

    Update finger.php
    added code for saving identity info to file that can be used with account creation
    currently saves in plaintext...might be a liability, but assuming that file will exist for only short time since it will be consumed and then deleted during account creation.

--- finger.php
+++ finger.php
@@ -13,20 +13,34 @@ Identity data:&nbsp;
 <p>
 From location:&nbsp;
 <?php print $Location; ?>
+
 <?php 
 if ($Location == "admin") {
-	echo "<body style='background-color:blue'>";
-} elseif ($Identity == "007") {
-	echo "<body style='background-color:green'>";
-} else {
-	echo "<body style='background-color:red'>";
-}
-?>
-<?php 
-if ($Location == "admin") {
-	//save $Identity to buffer for account creation;
-	echo "<body style='background-color:blue'>";
-	print "Print stored for Account Creation";
+	//produce file that stores $Identity for account creation
+	$saved = false;
+	$Name = rand();
+	while (!$saved)
+	{
+		//attempt create file for writing
+		$newID = fopen("newfinger\\" + $Name + ".txt", x);
+		if (!$newID)	//successful new file
+		{
+			//wrtie $Identity into file
+			fwrite($newID, $Identity);
+			//display filename for use for account creation
+			echo "<body style='background-color:blue'>";
+			print "Print stored for Account Creation.\r";
+			print "Code: " + $Name;
+			//close file and mark successful save so loop stops
+			fclose($newID);
+			$saved = true;
+		}
+		else	//tried to create using file name that already exists
+		{
+			//setup new name for reattempt in next loop
+			$Name = rand();
+		}
+	} //assume that account creation will consume and delete file
 } elseif (/* checkin($Identity, $Location) */) {
 	echo "<body style='background-color:green'>";
 	print "You are marked as attended.";
