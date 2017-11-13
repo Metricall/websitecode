04d3c38473e97209aeb5780beb73e5eecf550a59
wunami <wunami@users.noreply.github.com>
2017-11-12 23:29:19 -0800

    Update finger.php
    Fixed admin location fingerprint file creation.  Should not create thousands of files in a loop anymore.

--- finger.php
+++ finger.php
@@ -17,31 +17,33 @@ From location:&nbsp;
 <?php 
 if ($Location == "admin") {
 	//produce file that stores $Identity for account creation
-	$saved = false;
-	$Name = rand();
-	while (!$saved)
+	$Name = rand(100,999);
+	$Attempts = 0;
+	//find a filename that doesn't exist
+	while (file_exists("fingerlog" . $Name . ".txt") AND ++$Attempts < 1000)
+		$Name = rand(100,999);
+	
+	if ($Attempts < 1000)
 	{
 		//attempt create file for writing
-		$newID = fopen("newfinger\\" + $Name + ".txt", x);
-		if (!$newID)	//successful new file
-		{
-			//wrtie $Identity into file
-			fwrite($newID, $Identity);
-			//display filename for use for account creation
-			echo "<body style='background-color:blue'>";
-			print "Print stored for Account Creation.\r";
-			print "Code: " + $Name;
-			//close file and mark successful save so loop stops
-			fclose($newID);
-			$saved = true;
-		}
-		else	//tried to create using file name that already exists
-		{
-			//setup new name for reattempt in next loop
-			$Name = rand();
-		}
-	} //assume that account creation will consume and delete file
-} elseif (/* checkin($Identity, $Location) */) {
+		$newID = fopen("fingerlog" . $Name . ".txt", "w");
+		//wrtie $Identity into file
+		fwrite($newID, $Identity);
+		//display filename for use for account creation
+		echo "<body style='background-color:blue'>";
+		print "<br><br>Biometric identity information stored for Account Creation.\r";
+		print "<br><br>Code: " . $Name;
+		//close file and mark successful save so loop stops
+		fclose($newID);
+		//assume that account creation will consume and delete file
+	}
+	else
+	{
+		echo "<body style='background-color:red'>";
+		print "<br><br>ERROR storing biometric identity information.";
+		print "<br>Try cleaning utility to clear old files.";
+	}
+} elseif (true /* checkin($Identity, $Location) */) {
 	echo "<body style='background-color:green'>";
 	print "You are marked as attended.";
 } else {
