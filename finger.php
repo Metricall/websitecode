87ec3e465189ed9ca83b3eb81c76467fb780ea0b
wunami <wunami@users.noreply.github.com>
2017-11-13 16:41:31 -0800

    Update finger.php
    added function for checking if scanned ID data matches someone in the session currently.  it has assumptions about what the database utility function will be called and their parameters.  will reconcile once those getters and setters are in place.
    
    the logic should work, but difficult to test without actual rosters and sessions available.

--- finger.php
+++ finger.php
@@ -4,16 +4,66 @@
 </head>
 <?php $Identity = $_REQUEST['Identity'] ; $Location = $_REQUEST['Location'] ; ?>
 
+<?php
+function checkin($id, $loc) {
+	//trying to find current session, start as false
+	$CurrentSession = false;
+
+	//get session list for location from database and make into array
+//	$SessionString = getSessions($loc);
+	$SessionString = "12313,347667,6578456,4564577867,56754534";
+	$SessionList = explode(',', $SessionString);
+	
+	//check date and times to see if it is current session
+	foreach ($SessionList as $ThisSession)
+	{
+		if (getSessionDate($ThisSession) == date("Y-m-d"))
+		{
+			if ( date("h:i:sa") >= getSessionStart($ThisSession) AND date("h:i:sa") <= getSessionEnd($ThisSession)
+			{
+				$CurrentSession = $ThisSession;
+				break;
+			}
+		}
+	}
+	
+	//if $CurrentSession remains false, then no current session found, return false
+	if($CurrentSession == false)
+		return false;
+	//if there is a $CurrentSession, try to match $id against list of students for that session
+	else
+	{
+		//obtain student list for roster that this session is part of and make into array
+		$StudentString = getStudentList(getRosterID($CurrentSession));
+		$StudentList = explode(',', $StudentString);
+		
+		//check current $id against stored ids of each student
+		foreach ($StudentList as $ThisStudent)
+		{
+			//if match found, then add student to 'attended' for this session
+			if($id == getStudentIdentity($ThisStudent))
+			{
+				//obtain attended and make into array
+				$AttendedString = getAttended($CurrentSession);
+				$AttendedList = explode(',', $AttendedString);
+				//add current student to array
+				$AttendedList[] = $ThisStudent;
+				//construct new string with current student added
+				$AttendedString = implode(',', $AttendedList);
+				//replace attended for this session with new string containing this student
+				setAttended($CurrentSession, $AttendedString);
+				return true;
+			}
+		}
+		return false;
+	}
+}
+?>
+
 <body bgcolor="#FFFFFF" text="#000000">
 <center>
 <br><br><br>
-<p>
-Identity data:&nbsp;
-<?php print $Identity; ?>
-<p>
-From location:&nbsp;
-<?php print $Location; ?>
-
+<font size="500%" face="verdana" color="white">
 <?php 
 if ($Location == "admin") {
 	//produce file that stores $Identity for account creation
@@ -31,8 +81,8 @@ if ($Location == "admin") {
 		fwrite($newID, $Identity);
 		//display filename for use for account creation
 		echo "<body style='background-color:blue'>";
-		print "<br><br>Biometric identity information stored for Account Creation.\r";
-		print "<br><br>Code: " . $Name;
+		print "<br><br>Account Creation Biometric Information stored.\r";
+		print "<br>Remember Your Code: " . $Name;
 		//close file and mark successful save so loop stops
 		fclose($newID);
 		//assume that account creation will consume and delete file
@@ -41,16 +91,19 @@ if ($Location == "admin") {
 	{
 		echo "<body style='background-color:red'>";
 		print "<br><br>ERROR storing biometric identity information.";
-		print "<br>Try cleaning utility to clear old files.";
+		print "<br>(Try again or try using cleaning utility to clear old files.)";
 	}
-} elseif (true /* checkin($Identity, $Location) */) {
+} elseif (checkin($Identity, $Location)) {
 	echo "<body style='background-color:green'>";
-	print "You are marked as attended.";
+	print "Attended!";
 } else {
 	echo "<body style='background-color:red'>";
 	print "Attendance checkin failed.";
 }
 ?>
+</font>
+<br><br><br><br><br><br><br>
+<?php print "i: " . $Identity . " | l: " . $Location; ?>
 <center>
 <p>
 </body>
