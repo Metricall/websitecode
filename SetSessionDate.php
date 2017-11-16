383541b58854b129f468f5f258dad645100a63c4
techproph <33263490+techproph@users.noreply.github.com>
2017-11-16 01:21:12 -0800

    Update SetSessionDate.php

--- SetSessionDate.php
+++ SetSessionDate.php
@@ -10,7 +10,7 @@
 </head>
 <?php
 
-if (isset($_POST["Rost_ID"])){ //If it is the first time, it does nothing
+if (isset($_POST["Session_ID"])){ //If it is the first time, it does nothing
     SubmitSessionDate();
 }
 ?>
@@ -19,7 +19,7 @@ if (isset($_POST["Rost_ID"])){ //If it is the first time, it does nothing
 
 <Form name ="form2" Method ="Post" Action ="SetSessionDate.php?func_name=SubmitSessionDate">
 
-<div class = 'col-xs-3'><INPUT TYPE = "TEXT"  Name ="Rost_ID"></div>
+<div class = 'col-xs-3'><INPUT TYPE = "TEXT"  Name ="Session_ID"></div>
 <div class = 'col-xs-3'><INPUT TYPE = "TEXT"  Name ="Date"></div>
 
 <div class = 'col-xs-1'><INPUT TYPE = "Submit" Name = "Set_Session_Date" VALUE = "Update"></div>
@@ -27,10 +27,10 @@ if (isset($_POST["Rost_ID"])){ //If it is the first time, it does nothing
 <?PHP
 function SubmitSessionDate(){
     include 'DatabaseUtilities.php';
-    $the_Roster_Id = $_POST['Rost_ID'];
+    $the_Session_Id = $_POST['Session_ID'];
     $the_Date = $_POST['Date'];
     echo "Calling function";
-    SetSessionDate($the_Roster_Id, $the_Date);
+    SetSessionDate($the_Session_Id, $the_Date);
     
 }
 ?>
