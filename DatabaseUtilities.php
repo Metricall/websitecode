c1f8e273c1170d2ca23606ac6074e2a4e837691a
mattaffa <32315417+mattaffa@users.noreply.github.com>
2017-11-15 22:37:33 -0800

    Update DatabaseUtilities.php

--- DatabaseUtilities.php
+++ DatabaseUtilities.php
@@ -1,4 +1,10 @@
 <?PHP
+
+//--------------------------------------------------------
+//SET FUNCTIONS
+//--------------------------------------------------------
+
+
 function setStudentList($rosterID, $studentList){
 include 'DatabaseInfo.php';
 // Create connection
@@ -8,7 +14,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $sql = "UPDATE Roster SET Student_List = '".$studentList."' WHERE Roster_ID = '".$rosterID."'";
-$result = mysqli_query($conn, $sql);
+//$result = mysqli_query($conn, $sql);
 
 if (mysqli_query($conn, $sql)) {
     echo "Record updated successfully";
@@ -19,6 +25,51 @@ mysqli_close($conn);
 	
 }
 
+function setCourseName($the_Roster_ID, $the_Course_Name){
+include 'DatabaseInfo.php';
+// Create connection
+$conn = mysqli_connect($DB_servername, $DB_username, $DB_password, $DB_name);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "UPDATE Roster SET Course_Name = '".$the_Course_Name."' WHERE Roster_ID = '".$the_Roster_ID."'";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+	
+}
+
+
+//--------------------------------------------------------
+//ADD FUNCTIONS
+//--------------------------------------------------------
+function addNewRoster($Roster_ID, $Instructor_ID, $LocationID) {
+include 'DatabaseInfo.php';
+// Create connection
+$conn = mysqli_connect($DB_servername, $DB_username, $DB_password, $DB_name);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO Roster (Roster_ID, Course_Name, Student_List, Instructor_ID, Default_Location) VALUES ('".$Roster_ID."', '', '', '".$Instructor_ID."', '".$LocationID."')";
+if (mysqli_query($conn, $sql)) {
+    echo "Record added successfully";
+} else {
+    echo "Error adding record: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+	
+}	
+
+
+//--------------------------------------------------------
+//GET FUNCTIONS
+//--------------------------------------------------------
 function getStudentList($rosterID){
 include 'DatabaseInfo.php';
 // Create connection
@@ -66,19 +117,27 @@ if (mysqli_num_rows($result) > 0)  {
 mysqli_close($conn);
 }
 
+function getInstructorIDByName($Fname, $Lname) {
+include 'DatabaseInfo.php';
+// Create connection
+$conn = mysqli_connect($DB_servername, $DB_username, $DB_password, $DB_name);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}	
+$sql = "SELECT Std_ID FROM Users WHERE Fname = '".$Fname."' AND Lname = '".$Lname."'";
+$result = mysqli_query($conn, $sql);	
+if (mysqli_num_rows($result) > 0)  {
+	$query = mysqli_fetch_assoc($result);
+	$the_ID = $query["Std_ID"];
+	return $the_ID;
+	mysqli_close($conn);
+	exit;
+}
+ else {
+    echo "Error retreiving record: " . mysqli_error($conn);
+	return false;
+}
+mysqli_close($conn);
+}		
+
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
