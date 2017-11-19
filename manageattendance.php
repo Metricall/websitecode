f24373b0a40e97de2d26e8b1d596cf20a45f8db7
wunami <wunami@users.noreply.github.com>
2017-11-19 15:55:51 -0800

    Add files via upload
    Choosing different professor class options now changes the form on the page to the appropriate one from the UI team (non-functional still).

---
+++ manageattendance.php
@@ -0,0 +1,78 @@
+	<div class = 'row' id = 'yourclasses'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-7'><h2>Spring 2018</h2></div>
+	</div><br>
+	<table border="1" id = 'roster'>
+  <tr>
+  <th></th>
+    <th id = 'color11'>Name</th>
+    <th id = 'color22'>StudentID</th>
+	<th id = 'color33'>1/23</th>
+    <th id = 'color44'>1/30</th>
+	<th id = 'color33'>2/06</th>
+    <th id = 'color44'>2/13</th>
+	<th id = 'color33'>2/20</th>
+    <th id = 'color44'>2/27</th>
+	<th id = 'color33'>3/06</th>
+    <th id = 'color44'>3/13</th>
+	<th id = 'color33'>3/20</th>
+    <th id = 'color44'>3/27</th>
+	<th id = 'color33'>4/05</th>
+    <th id = 'color44'>4/12</th>
+	<th></th>
+  </tr>
+  <tr>
+  <td><input type = "button" value = 'edit' id ='editstudent1' onclick ='editstudent()'></td>
+	<td id = 'color11'>Matthew Affa</td>
+	<td id = 'color22'>123456</td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent" ></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type = "button" value = 'save' id = 'savestudent1' onclick ='savestudent()'></td>
+  </tr>
+  <tr>
+  <td><input type = "button" value = 'edit' id ='editstudent2'></td>
+	<td id = 'color11'>Crystele Dierickx</td>
+	<td id = 'color22'>567890</td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent" ></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type = "button" value = 'save' id = 'savestudent2'></td>
+  </tr>
+   <tr>
+  <td><input type = "button" value = 'edit' id ='editstudent3'></td>
+	<td id = 'color11'>Victor Zavala</td>
+	<td id = 'color22'>987654</td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent" ></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type="checkbox" disabled id="absent"></td>
+	<td align = 'center'><input type = "button" value = 'save' id = 'savestudent3'></td>
+  </tr>
+</table>
---
+++ session.php
@@ -0,0 +1,44 @@
+	<div class = 'row' id = 'yourclasses'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-7'><h2>Create a Session</h2></div>
+	</div><br>
+	<div class = 'row' id = 'firstform'>
+	<div class = 'col-xs-3'></div>
+	<div class = 'col-xs-2'> Session Name </div>
+	<div class = 'col-xs-1'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'lastform'>
+	<div class = 'col-xs-3'></div>
+	<div class = 'col-xs-2'> Session Date </div>
+	<div class = 'col-xs-1'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'emailform'>
+	<div class = 'col-xs-3'></div>
+	<div class = 'col-xs-2'> Session From </div>
+	<div class = 'col-xs-1'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'messageform'>
+	<div class = 'col-xs-3'></div>
+	<div class = 'col-xs-2'> Session To </div>
+	<div class = 'col-xs-1'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'sendbutton'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-3'><input type = 'submit' id = 'submitform' value = 'Create'></div>
+	</div><br><br><br>
+	
+	<table border='1' id = 'roster'>
+  <tr>
+    <th id = 'color11'>Session ID</th>
+    <th id = 'color22'>Session Name</th>
+	<th id = 'color11'>Date</th>
+    <th id = 'color22'>from</th>
+	<th id = 'color11'>to</th>
+  </tr>
+  <tr>
+	<td id = 'color11'>11111</td>
+	<td id = 'color22'>Test # 1</td>
+	<td id = 'color11'>March 21, 2018</td>
+	<td id = 'color22'>10:30 AM</td>
+	<td id = 'color22'>12:00 PM</td>
+</table>
