c5f562e8ec1b2d7ea3da6638ac059498015f2d6e
wunami <wunami@users.noreply.github.com>
2017-11-21 01:08:07 -0800

    Update and rename adminmain.html to adminmain.php
    login functionality added.
    used includes to cover header/menu/footer common on all admin pages.

---
+++ adminmain.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+<?php
+	include 'LoginUtilities.php';
+	if (checkLogin()) {
+		//not an admin
+		if ($_SESSION["role"] != "Admin")
+			header("Location: logout.php");
+		//setup adminID
+		elseif($_SESSION["role"] == "Admin")
+			$_SESSION["adminID"] = $_SESSION["uid"];
+	}
+?>
+<?php
+	include 'DatabaseUtilities.php';
+	$fn = getUserFirstName($_SESSION["adminID"]);
+	$ln = getUserLastName($_SESSION["adminID"]);
+	$_SESSION["adminName"] =  "{$fn} {$ln}";
+?>
+<!DOCTYPE html>
+<!--admin
+ -->
+<html lang = "en">
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+	<link rel = "stylesheet" type = "text/css" href = "roster.css">
+    <title> Administrator </title>
+    <meta charset = "utf-8">
+  </head>
+  <body>
+	<?php include 'header.php'; ?>
+	<?php include 'adminmenu.php'; ?>
+	<?php include 'footer.php'; ?>
+  </body>
+</html>
