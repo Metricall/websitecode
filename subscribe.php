5464525bc79fa397ae2e37cd66a2824a1c1eefa4
wunami <wunami@users.noreply.github.com>
2017-11-22 22:51:24 -0800

    Update and rename subscribe.html to subscribe.php
    convert to PHP to make use of header/footer includes.

---
+++ subscribe.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--contact us
+ -->
+<html lang = "en">
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'backcolor1'><br>
+	<div class = 'row' id = 'backcolor'>
+	<br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-4'></div>
+	<div class = 'col-xs-4'><h2>Subscribe</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div><br>
+	<div class = 'row' id = 'firstform'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-2'> First </div>
+	<div class = 'col-xs-6'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'lastform'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-2'> Last </div>
+	<div class = 'col-xs-6'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'emailform'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-2'> email </div>
+	<div class = 'col-xs-6'><input type = 'text' id = 'box'></div>
+	</div><br>
+	<div class = 'row' id = 'sendbutton'>
+	<div class = 'col-xs-4'></div>
+	<div class = 'col-xs-3'><input type = 'submit' id = 'submitform' value = 'Send'></div>
+	</div><br><br><br>
+	</div><br>
+	</div>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
