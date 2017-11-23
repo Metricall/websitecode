eb74524d883ae125a3a1058da843cceae9510d8f
wunami <wunami@users.noreply.github.com>
2017-11-22 22:50:05 -0800

    Update and rename resources.html to resources.php
    convert to PHP to make use of header/footer includes.

---
+++ resources.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--resources
+ -->
+<html lang = "en">
+ <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>Resources</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div>
+	<div class = 'row' id = 'resources1'>
+	</div>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-2'></div>
+	<div class = 'col-xs-8'><h2>One of our main goals is to get to a point where
+	                            we dont have to use much resources from the planet.</h2></div>
+	<div class = 'col-xs-2'></div>
+	</div>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
