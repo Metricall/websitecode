0010177c30701595c5b5c73b0932bd9cb681a59d
wunami <wunami@users.noreply.github.com>
2017-11-22 22:49:20 -0800

    Update and rename products.html to products.php
    convert to PHP to make use of header/footer includes.

---
+++ products.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--products
+ -->
+<html lang = "en">
+ <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'add a pic'>
+	
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>Products</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div>
+	<div class = 'row' id = 'content1'>
+	<div class = 'col-xs-3'></div>
+	<div class = 'col-xs-6'>Team metrical offers a variety of products focused on making the everyday life easier for any
+	type of business. Our main goal is to save employers time allowing them to focus their attention on 
+	different areas in need of development. </div>
+	<div class = 'col-xs-3'></div>
+	</div><br>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><img src = 'location.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'bio.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'house.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	</div>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><h3>Location</h3></div>
+	<div class = 'col-xs-2'><h3>Biometrics</h3></div>
+	<div class = 'col-xs-2'><h3>Biounlock</h3></div>
+	<div class = 'col-xs-2'><h3>Sales</h3></div>
+	</div>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><h5>With our location services you can locate an employee in seconds</h5></div>
+	<div class = 'col-xs-2'><h5>Our biometrics service offers secure and fast role taking</h5></div>
+	<div class = 'col-xs-2'><h5>Unlock your business or your home with only one touch on your phone</h5></div>
+	<div class = 'col-xs-2'><h5>Services for grocery stores, allow their VIP clients to pay with just a finger print</h5></div>
+	</div><br><br>
+	</div>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
