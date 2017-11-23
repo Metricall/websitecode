0998dd845efcb9b3e69537f4b688b27b1efc6f93
wunami <wunami@users.noreply.github.com>
2017-11-22 22:50:49 -0800

    Update and rename socialmedia.html to socialmedia.php
    convert to PHP to make use of header/footer includes.

---
+++ socialmedia.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--contact us
+ -->
+<html lang = "en">
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>Social Media</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div>
+	 <div class = 'row' id = 'logo'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-3'><h2>Facebook</h2></div>
+	<div class = 'col-xs-4'><a href="https://www.facebook.com"><img src = 'facebook.svg' width = '80px' ></a></div>
+	<div class = 'col-xs-5'></div>
+	</div>
+	 <div class = 'row' id = 'logo'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-3'><h2>Twitter </h2></div>
+	<div class = 'col-xs-4'><a href ="https://www.twitter.com"><img src = 'twitter.png' width = '80px' ></a></div>
+	<div class = 'col-xs-5'></div>
+	</div><br><br><br>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
