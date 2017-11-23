b671b4fbc50c6714329c4ea3804220d53b963b63
wunami <wunami@users.noreply.github.com>
2017-11-22 22:48:39 -0800

    Update and rename news.html to news.php
    convert to PHP to make use of header/footer includes.

---
+++ news.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--news
+ -->
+<html lang = "en">
+ <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>News</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div><br>
+	
+	 <div class = 'row' id = 'logo'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-3'><h4>Increase your business revenue</h4></div>
+	<div class = 'col-xs-4'><a href = 'https://personalmba.com/4-methods-to-increase-revenue/'><img src = 'bus1.jpg' width = '200px' ></a></div>
+	<div class = 'col-xs-5'></div>
+	</div>
+	 <div class = 'row' id = 'logo'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-3'><h4>Biometrics a secure way for a business </h4></div>
+	<div class = 'col-xs-4'><a href = 'http://www.gemalto.com/govt/inspired/biometrics'><img src = 'bus2.jpg' width = '200px' ></a></div>
+	<div class = 'col-xs-5'></div>
+	</div>
+	<div class = 'row' id = 'logo'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-3'><h4>Sales service for a business </h4></div>
+	<div class = 'col-xs-4'><a href = 'https://www.statista.com/statistics/522058/global-smartphone-fingerprint-penetration/'><img src = 'bus3.jpg' width = '200px' ></a></div>
+	<div class = 'col-xs-5'></div>
+	</div>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
