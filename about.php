e468dbb7a48925356a5345ec65c0f7f101cf7a7f
wunami <wunami@users.noreply.github.com>
2017-11-22 22:42:50 -0800

    Update and rename about.html to about.php
    convert to PHP to make use of header/footer includes.

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--about
+ -->
+<html lang = "en">
+ <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'add a pic'>
+	
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>About Us</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div>
+	<div class = 'row' id = 'teamback'></div>
+	
+	</div><br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-1'></div>
+	<div class = 'col-xs-10'>In metrical our main purpose is to make a business suceed by making 
+	                         their everyday life easier and without complications. We build solutions 
+							 for any type of business that wants to have a more organized and secure 
+							 work envorinment. From employees to customers we have the right products 
+							 for everyone.</div>
+	<div class = 'col-xs-1'></div>
+	</div><br>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
+	
