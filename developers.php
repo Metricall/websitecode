9eac70c27de495c2945a16f3fa0560d38b6b1739
wunami <wunami@users.noreply.github.com>
2017-11-22 22:45:37 -0800

    Update and rename developers.html to developers.php
    convert to PHP to make use of header/footer includes.

---
+++ developers.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--about
+ -->
+<html lang = "en">
+ <head>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
+	<link rel = "stylesheet" type = "text/css" href = "mainmetrical.css">
+    <title> Metrical </title>
+    <meta charset = "utf-8">
+ </head>
+ <body>
+	<?php include 'header.php'; ?>
+	<br>
+	<div class = 'row' id = 'team'>
+	<div class = 'col-xs-5'></div>
+	<div class = 'col-xs-4'><h2>Team Metrical</h2></div>
+	<div class = 'col-xs-3'></div>
+	</div><br>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	</div>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><h3>Crystele</h3></div>
+	<div class = 'col-xs-2'><h3>John</h3></div>
+	<div class = 'col-xs-2'><h3>Matt</h3></div>
+	<div class = 'col-xs-2'><h3>Victor</h3></div>
+	</div>
+	<div class = 'row' id = 'teamnames'>
+	<div class = 'col-xs-2' id = 'crystele'><h4>Product Owner</h4></div>
+	<div class = 'col-xs-2' id = 'john'><h4>Project Manager</h4></div>
+	<div class = 'col-xs-2' id = 'matt'><h4>Db Administrator</h4></div>
+	<div class = 'col-xs-2' id = 'victor'><h4>UI Developer</h4></div>
+	</div>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	<div class = 'col-xs-2'><img src = 'person.png' width = '80px' ></div>
+	</div>
+	<div class = 'row' id = 'teamnames2'>
+	<div class = 'col-xs-2'><h3>Jason</h3></div>
+	<div class = 'col-xs-2'><h3>Michael</h3></div>
+	<div class = 'col-xs-2'><h3>Alejandro</h3></div>
+	</div>
+	<div class = 'row' id = 'teamnames'>
+	<div class = 'col-xs-2' id = 'jason'><h4>UI Developer</h4></div>
+	<div class = 'col-xs-2' id = 'michael'><h4>DB Developer</h4></div>
+	<div class = 'col-xs-2' id = 'alejandro'><h4>DB Developer</h4></div>
+	</div>
+	<?php include 'footer.php'; ?>
+	</body>
+</html>
+	
